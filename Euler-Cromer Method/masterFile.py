a4236a70cff335bd1939ebb3b1aa68140cd962be
Bradon-Timms <bradon.timms@gmail.com>
2023-10-18 13:23:05 -0400

    Updated masterFile

--- Euler-Cromer Method/masterFile.py
+++ Euler-Cromer Method/masterFile.py
@@ -1,7 +1,7 @@
 from thrustProfile import thrustProfile
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 G = 6.674e-11
 M_earth = 5.972e24
 airDensity = 1.293 #kg/m^3
@@ -12,7 +12,7 @@ v = v0
 z = z0
 V = v
 Z = z
-m = 1
+m = 18.7674
 M = m
 tmax = 100
 Cd = 0.3
@@ -24,6 +24,8 @@ def dm (time, z):
         return (3.54369/2.6)
     elif time>50 and z == 500:
         return 3.40194
+    else:
+        return 0
     
 def Cd(time, z):
     if time> 10 and z <= 213.36:
@@ -31,14 +33,26 @@ def Cd(time, z):
     else:
         return 0.3
 t = np.linspace(0,tmax,1001)
+def area(time, z):
+    if time> 10 and z <= 213.36:
+        return (3.048/2)*(3.048/2)*math.pi
+    else:
+        return (0.131318/2)*(0.131318/2)*math.pi
 for time in range(0,999):
     m = m - (dm(t[time], z)*dt)
-    drag = 0.5 *airDensity * v * v *Cd(t[time], z)
+    drag = 0.5 *airDensity * v * v *Cd(t[time], z)*area(t[time], z) / m
     if v < 0:
         drag = drag*-1
-    v = v + (thrustProfile(t[time]) - drag - g)*dt
-    print(v)
+    v = v + (thrustProfile(t[time])/m - drag - g)*dt
     z = z + v*dt
+    print("drag")
+    print(drag)
+    print("velocity")
+    print(v)
+    print("time")
+    print(t[time])
+    print("height")
+    print(z)
     altitude.append(z)
     velocity.append(v)
     if z <0:
