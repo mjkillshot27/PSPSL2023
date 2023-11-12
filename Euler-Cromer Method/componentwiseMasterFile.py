cb49a575614f3d1c120e9baeec6f33744ddc8c6a
Bradon-Timms <bradon.timms@gmail.com>
2023-11-12 12:18:28 -0500

    updates with x and y coefficients

--- Euler-Cromer Method/componentwiseMasterFile.py
+++ Euler-Cromer Method/componentwiseMasterFile.py
@@ -12,6 +12,11 @@ v = v0
 z = z0
 V = v
 Z = z
+vx = 0
+vy = 0
+launchAngle = 90
+angle = launchAngle
+windSpeed = 0
 m = 18.7674 #initial mass
 M = m
 tmax = 100
@@ -31,24 +36,36 @@ def Cd(time, z):
     if time> 10 and z <= 213.36:
         return 1.6
     else:
-        return 0.3
+        return 0.453
 t = np.linspace(0,tmax,1001)
 def area(time, z):
     if time> 10 and z <= 213.36:
         return (3.048/2)*(3.048/2)*math.pi
     else:
         return (0.131318/2)*(0.131318/2)*math.pi
-for time in range(0,999):
+for time in range(0,300):
     m = m - (dm(t[time], z)*dt)
     parachuteDeploymentCounter = 0
     if z<=213.36  and time>200 and parachuteDeploymentCounter == 0:
         v = -3
         parachuteDeploymentCounter = parachuteDeploymentCounter +1
+    
     drag = 0.5 *airDensity * v * v *Cd(t[time], z)*area(t[time], z) / m
+    print(drag)
     if v < 0:
         drag = drag*-1
     v = v + (thrustProfile(t[time])/m - drag - g)*dt
-    z = z + v*dt
+    vx = (v + (thrustProfile(t[time])/m - drag - g))*np.cos(angle * np.pi/180)*dt
+    vy = (v + (thrustProfile(t[time])/m - drag - g))*np.sin(angle * np.pi/180)*dt
+    if vx == 0:
+        angle = 90
+    else:
+        angle = np.arctan(vy/vx)
+    print("m",m)
+    print("v",v)
+    print("vx", vx)
+    print("vy", vy)
+    z = z + vy*dt
     print("drag")
     print(drag)
     print("velocity")
@@ -58,7 +75,7 @@ for time in range(0,999):
     print("height")
     print(z)
     altitude.append(z*3.281)
-    velocity.append(v*3.281)
+    velocity.append(vy*3.281)
     if z <0:
         break
 altitude.append(1)
