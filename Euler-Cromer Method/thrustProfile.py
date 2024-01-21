6214b678723b5cdb97e988ce149d1eb1fcfc1cd2
RyanSamF <157307644+RyanSamF@users.noreply.github.com>
2024-01-21 12:51:45 -0500

    Update thrustProfile.py

--- Euler-Cromer Method/thrustProfile.py
+++ Euler-Cromer Method/thrustProfile.py
@@ -1,7 +1,7 @@
 def thrustProfile(time):   
     thrustTimes = [0, 0.00776398, 0.0465839, 0.48913, 1.01708, 1.99534, 2.3913, 2.48447, 2.50776, 2.6]
     thrustValues = [0, 1344.83, 1241.38, 1551.72, 1568.97, 1603.45, 1586.21, 1758.62, 103.448, 0]
-    if time <= time[len(time) - 1]:
+    if time <= thrustTimes[len(thrustTimes) - 1]:
         for i in range(len(thrustTimes)-1):
             if time > thrustTimes[i]:
                 fromPoint = time - thrustTimes[i]
